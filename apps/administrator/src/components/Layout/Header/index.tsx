8758c25e67f44859cd792336c18219a217b1eab0
Lara99n <91375775+Lara99n@users.noreply.github.com>
2023-01-30 23:40:01 -0300

    styles: navigation and header styles ready

--- apps/administrator/src/components/Layout/Header/index.tsx
+++ apps/administrator/src/components/Layout/Header/index.tsx
@@ -1,8 +1,51 @@
+import React, { useState } from "react";
+
+import styles from "./header.module.css";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
+  const [openMenu, setOpenMenu] = useState(false);
 
-export default Header
+  /*   const admin = localStorage.getItem("admin")
+    ? JSON.parse(localStorage.getItem("admin") || "")
+    : "";
+ */
+  return (
+    <header>
+      <nav>
+        <div className={`${styles.dropdown} ${styles.active}`}>
+          <div className={styles.container_info_header}>
+            <div className={styles.notification_container}>
+              <img src="./newAssets/icono-notificacion.svg" />
+            </div>
+
+            <div className={styles.header_user_info}>
+              <h3
+                className={styles.user_name}
+                onClick={() => setOpenMenu(!openMenu)}
+              >
+                {/*                 {admin.name} {admin.lastname}
+                 */}
+                Lara Albornoz
+              </h3>
+              <div className={styles.user_image}>
+                <img src="default.jpg" alt="" />
+              </div>
+            </div>
+          </div>
+
+          <div className={openMenu ? styles.open_menu : styles.close_menu}>
+            <div className={styles.menu_content}>
+              <Link to="/settings">Mi perfil</Link>
+              <Link to="/settings-business">Configurar negocio</Link>
+              <button>Cambiar de negocio</button>
+              <button>Cerrar Sesion</button>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
