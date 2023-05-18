f2eacce80965cf6a31b43f08a0e1370c77700bd8
NestorMosqueraBmatch <nestor@bmatch.cl>
2023-05-18 11:44:31 -0500

    chore: config rush monorepo and build statemen

---
+++ packages/constant-definitions/src/functions/data-sources-helpers/constants.ts
@@ -0,0 +1,12 @@
+export enum Collection{
+ ADMINS = 'admins',
+ COVERS = 'covers',
+ GOERS = 'goers',
+ STORES = 'stores',
+ USERS = 'users',
+ WAITERS = 'waiters',
+ COMMENTS = 'comments',
+ FOLLOWS = 'follows',
+ BOOKINGS = 'bookings',
+ REPORTED = 'reported'
+}
