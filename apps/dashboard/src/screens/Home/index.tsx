24a050d9d066fb2b2f7ecdcd314bb9b61bea1bbd
jordislpm <jordislpm@gmail.com>
2023-06-05 11:32:58 -0400

    feat: login screen

--- apps/dashboard/src/screens/Home/index.tsx
+++ apps/dashboard/src/screens/Home/index.tsx
@@ -1,13 +1,14 @@
-import { Store } from '@partiaf/entities';
 import Button from '@/components/Shared/Button';
 import { Card } from '@/components/UI/Home';
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { PrivateRoutes } from '@/constant-definitions';
+import Login from '../Login';
 
 const Home = () => {
   return (
     <div>
+      <Login/>
       <Button>Partiaf Button</Button>
       <Card />
     </div>
