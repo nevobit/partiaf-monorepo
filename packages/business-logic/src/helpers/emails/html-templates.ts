f099e96c6363389dd5126aa186d1f7b01dfa0488
Yixsi <yixsi3103@gmail.com>
2023-06-05 13:07:31 -0500

    feat: added mail notifications on created accounts

--- packages/business-logic/src/helpers/emails/html-templates.ts
+++ packages/business-logic/src/helpers/emails/html-templates.ts
@@ -30,11 +30,8 @@ export const EmailSubjects = {
 export const EmailTemplates = {
   verification: `
     <div style=" padding: 1rem 2rem;">
-      <h3 style="font-size: 1.2rem; color: rgba(0,0,0, 0.8);">Verificar la dirección de correo electrónico</h3>
-      <p style="font-size: 14px;">Gracias por iniciar el proceso de creación de la nueva cuenta como administrador en Partiaf. Queremos asegurarnos de que es realmente usted. Por favor, ingrese el siguiente código de verificación cuando se lo solicite. Si no desea crear una cuenta, puede omitir este mensaje</p>
-      <h4 style="text-align: center; margin: 0; margin-top: 2rem; margin-bottom: 5px; color: rgba(0,0,0, 0.8);">Código de verificación</h4>
-      <h2 style="text-align: center; font-size: 2.2rem; margin: 0;">{code}</h2>
-      <p style="text-align: center; margin: 0; margin-bottom: 1rem; font-size: 14px; margin-top: 5px;">(Este código es válido durante 1 hora)</p>
+      <h3 style="font-size: 1.2rem; color: rgba(0,0,0, 0.8);">Confirmación de creación de cuenta</h3>
+      <p style="font-size: 14px;">Gracias por crear una cuenta en Partiaf. Inicia sesión y descubre todas las opciones que tenemos para ti.</p>
     </div>
   `,
 
