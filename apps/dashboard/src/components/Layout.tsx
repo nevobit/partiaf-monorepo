8cbe15a3675bbb466ef09448244dda072368f087
jordislpm <jordislpm@gmail.com>
2023-06-12 15:20:35 -0400

    feat: prepare layout and Reservation component

--- apps/dashboard/src/components/Layout.tsx
+++ apps/dashboard/src/components/Layout.tsx
@@ -1,5 +1,20 @@
-const Layout = () => {
-  return <></>;
-};
+import React from 'react'
+import Header from './Layout/Header'
+import Footer from './Layout/Footer'
+import Nav from './Layout/Nav'
+import Principal from './Layout/Principal'
 
-export default Layout;
+const Layout = ({children}: any) => {
+  return (
+    
+    <>
+      <Header/>
+      <Principal>
+       {children}
+      </Principal>
+      <Footer/>
+    </>
+  )
+}
+
+export default Layout
