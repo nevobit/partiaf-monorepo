6aec953eeeb575ad5419b7ff5edffee587a24429
Alessandro De Bonis <alessandrodebonis@MacBook-Air-de-Alessandro.local>
2023-07-15 20:39:34 -0500

    chore: update

--- apps/user/src/graphql/index.ts
+++ apps/user/src/graphql/index.ts
@@ -1,6 +1,6 @@
 import {ApolloClient, InMemoryCache, createHttpLink} from '@apollo/client';
 const httpLink = createHttpLink({
-  uri: 'http://192.168.1.1:8001/graphql',
+  uri: 'https://partiaf-api-graphql.xyz/graphql',
 });
 
 const client = new ApolloClient({
