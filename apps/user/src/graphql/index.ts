7f9d2f3e57b8e015c85faa9bade1cdee1937c3c5
NestorMosqueraBmatch <129396602+NestorMosqueraBmatch@users.noreply.github.com>
2023-07-20 13:59:39 -0500

    chore: add user app screen

--- apps/user/src/graphql/index.ts
+++ apps/user/src/graphql/index.ts
@@ -1,6 +1,7 @@
 import {ApolloClient, InMemoryCache, createHttpLink} from '@apollo/client';
 const httpLink = createHttpLink({
-  uri: 'http://192.168.1.62:8001/graphql',
+  // uri: 'http://192.168.1.62:8001/graphql',
+  uri: 'https://partiaf-api-graphql.xyz/graphql',
 });
 
 const client = new ApolloClient({
